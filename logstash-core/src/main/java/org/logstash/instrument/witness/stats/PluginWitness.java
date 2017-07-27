49c146c569bfaf219317bb3c8a68130c92a89687
Jake Landis <jake.landis@elastic.co>
2017-07-27 13:15:33 -0500

    wip: support for custom witness and plugins

--- logstash-core/src/main/java/org/logstash/instrument/witness/stats/PluginWitness.java
+++ logstash-core/src/main/java/org/logstash/instrument/witness/stats/PluginWitness.java
@@ -4,26 +4,46 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import org.logstash.instrument.metrics.gauge.TextGauge;
 import org.logstash.instrument.witness.SerializableWitness;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @JsonSerialize(using = PluginWitness.Serializer.class)
-final public class PluginWitness implements SerializableWitness {
+public class PluginWitness implements SerializableWitness {
 
 
     private final EventsWitness eventsWitness;
-    private final String KEY;
-    //TODO: inputs, output and filters
+    private final TextGauge id;
+    private final TextGauge name;
+    private final Map<Class<? extends SerializableWitness>, SerializableWitness > customWitnesses = new ConcurrentHashMap<>(1);
 
-
-    PluginWitness(String pluginName) {
-        KEY = pluginName;
-        this.eventsWitness = new EventsWitness();
+    public PluginWitness(String name) {
+        eventsWitness = new EventsWitness();
+        id = new TextGauge("id");
+        this.name = new TextGauge("name", name);
     }
 
     public EventsWitness event() {
         return eventsWitness;
+    }
+
+
+    public <T extends SerializableWitness> void addCustom(T witness) {
+        customWitnesses.putIfAbsent(witness.getClass(), witness);
+    }
+
+    /**
+     *
+     * @param clazz
+     * @param <T>
+     * @return null if a custom {@link SerializableWitness} of the provided type has not been added, else the custom {@link SerializableWitness}
+     */
+    public <T extends SerializableWitness> T custom(Class<T> clazz) {
+        SerializableWitness w = customWitnesses.get(clazz);
+        return  w == null ? null : clazz.cast(w);
     }
 
 
@@ -58,9 +78,12 @@ final public class PluginWitness implements SerializableWitness {
         }
 
         void innerSerialize(PluginWitness witness, JsonGenerator gen, SerializerProvider provider) throws IOException {
-            gen.writeObjectFieldStart(witness.KEY);
+            gen.writeStringField(witness.id.getName(), witness.id.getValue());
+            gen.writeStringField(witness.name.getName(), witness.name.getValue());
             witness.event().genJson(gen, provider);
-            gen.writeEndObject();
+            for (SerializableWitness customWitness : witness.customWitnesses.values()) {
+                customWitness.genJson(gen, provider);
+            }
         }
     }
 }
