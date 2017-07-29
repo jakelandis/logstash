cc9a817a4d6311dfd87ae0e231ac16501f39b7dd
Jake Landis <jake.landis@elastic.co>
2017-07-29 15:05:07 -0500

    wip: minor fixes

--- logstash-core/lib/logstash/instrument/wrapped_write_client.rb
+++ logstash-core/lib/logstash/instrument/wrapped_write_client.rb
@@ -1,7 +1,6 @@
 # encoding: utf-8
 java_import org.logstash.instrument.witness.Witness
 
-#WARNING - DO NOT USE THIS CLASS FOR ANYTHING BUT INPUT PLUGIN TYPES.  Using this for other plugin types will result doubly counted metrics.
 module LogStash module Instrument
   class WrappedWriteClient #todo: rename InputWrappedWriterClient
     def initialize(write_client, pipeline, metric, plugin, logger)
@@ -9,15 +8,17 @@ module LogStash module Instrument
       @logger = logger
 
       pipeline_id = pipeline.pipeline_id.to_s.to_sym
-      plugin_type = "#{plugin.class.plugin_type}s".to_sym
+      @plugin_type = "#{plugin.class.plugin_type}s".to_sym
 
       @events_metrics = metric.namespace([:stats, :events])#todo: delete
       @pipeline_metrics = metric.namespace([:stats, :pipelines, pipeline_id, :events]) #todo: delete
-      @plugin_events_metrics = metric.namespace([:stats, :pipelines, pipeline_id, :plugins, plugin_type, plugin.id.to_sym, :events]) #todo: delete
+      @plugin_events_metrics = metric.namespace([:stats, :pipelines, pipeline_id, :plugins, @plugin_type, plugin.id.to_sym, :events]) #todo: delete
 
-      @base_witness = Witness.getInstance()
-      @pipeline_witness = Witness.getInstance().pipeline(pipeline_id.to_s)
-      @plugin_witness = Witness.getInstance().pipeline(pipeline_id.to_s).input(plugin.id.to_s)
+      if (@plugin_type.eql? :inputs)
+        @base_witness = Witness.getInstance()
+        @pipeline_witness = Witness.getInstance().pipeline(pipeline_id.to_s)
+        @plugin_witness = Witness.getInstance().pipeline(pipeline_id.to_s).input(plugin.class.config_name)
+      end
 
 
       define_initial_metrics_values
@@ -43,9 +44,11 @@ module LogStash module Instrument
       @pipeline_metrics.increment(:in, size) #todo: delete
       @plugin_events_metrics.increment(:out, size) #todo: delete
 
-      @base_witness.event.in(size)
-      @pipeline_witness.event.in(size)
-      @plugin_witness.event.out(size)
+      if (@plugin_type.eql? :inputs)
+        @base_witness.event.in(size)
+        @pipeline_witness.event.in(size)
+        @plugin_witness.event.out(size)
+      end
 
       start_time = Time.now
       clock = @events_metrics.time(:queue_push_duration_in_millis) #todo: delete
@@ -60,8 +63,10 @@ module LogStash module Instrument
       @pipeline_metrics.report_time(:queue_push_duration_in_millis, execution_time)
       @plugin_events_metrics.report_time(:queue_push_duration_in_millis, execution_time)
 
-      @pipeline_witness.event.queuePushDuration((end_time - start_time)*1000)
-      @plugin_witness.event.queuePushDuration((end_time - start_time)*1000)
+      if (@plugin_type.eql? :inputs)
+        @pipeline_witness.event.queuePushDuration((end_time - start_time)*1000)
+        @plugin_witness.event.queuePushDuration((end_time - start_time)*1000)
+      end
 
 
       rescue => e
